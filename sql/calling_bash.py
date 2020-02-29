2572affa997ae3c19ac93e1293857c64274d5f5f
Cesar Zamora Martinez <cesar_czm@ciencias.unam.mx>
2020-02-29 11:20:59 -0600

    archivos para descargar rita

---
+++ sql/calling_bash.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import subprocess
+
+subprocess.call("./download_rita.sh", shell=True)
--- sql/download_rita.sh
+++ sql/download_rita.sh
@@ -2,17 +2,17 @@
 #! /bin/bash
 
 BASE_URL="https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_"
-
+BASE_ZIP="On_Time_Reporting_Carrier_On_Time_Performance_1987_present_"
 
 ## Esto linea deberia ser dinamica cuando se ejecute periodicamente
 
 YEAR=2019
 MONTH=11
 
-#url = ${BASE_URL}${YEAR}"\_"${MONTH}".zip"
-
+# Obtenemos el zip
 wget ${BASE_URL}${YEAR}"_"${MONTH}".zip"
-#echo ${url}
-#wget ${url}
 
-unzip On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2019_11.zip
+# Descomprimimos el zip, eliminando archivos remanentes
+unzip ${BASE_ZIP}${YEAR}"_"${MONTH}".zip" # Crea .csv
+rm ${BASE_ZIP}${YEAR}"_"${MONTH}".zip"
+rm 'readme.html'
