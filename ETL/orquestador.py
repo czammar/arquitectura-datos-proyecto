18c8f887ec4aa057c3eff95ac42482a41186c485
Cesar Zamora Martinez <cesar_czm@ciencias.unam.mx>
2020-02-29 16:42:00 -0600

    cambios a orquestador para que reconozca llaves

--- ETL/orquestador.py
+++ ETL/orquestador.py
@@ -1,8 +1,8 @@
-#! /home/vagrant/.pyenv/shims/python
+#! /Users/czammar/opt/anaconda3/bin/python
 # existe un bug con bot3 y luigi para pasar las credenciales
 # necesitas enviar el parámetro AWS_PROFILE e indicar el profile
 # con el que quieres que se corra
-# PYTHONPATH='.' AWS_PROFILE=dpa luigi --module orquestador.py S3Task --local-scheduler ...
+# PYTHONPATH='.' AWS_PROFILE=dpa luigi --module orquestador S3Task --local-scheduler ...
 import luigi
 import luigi.contrib.s3
 import boto3
@@ -11,15 +11,13 @@ import pyarrow
 import pyarrow.parquet as pq
 
 class LocalFileSystemTask(luigi.Task):
-#Parametros 
+#Parametros
 
 
     def run(self):
         with self.output().open('w') as output_file:
         	extract = exec(open('download_rita_parquet.py').read())
         	#output_file.open(extract)
-        
-
 
     def output(self):
         return luigi.local_target.LocalTarget('./On_Time_Reporting.parquet')
@@ -28,35 +26,27 @@ class LocalFileSystemTask(luigi.Task):
 class S3Task(luigi.Task):
 
     #Parametros
-    #task_name = "load"
-
-    bucket = luigi.Parameter()
-    #root_path = luigi.Parameter()
-    #year = luigi.Parameter()
-    #month = luigi.Parameter()
+    task_name = "load"
 
     def requires(self):
-        return LocalFileSystemTask(self)
+        return LocalFileSystemTask()
 
     def run(self):
-        ses = boto3.session.Session(profile_name="dpa", region_name='us-west-2')
+        ses = boto3.session.Session(profile_name='dpa', region_name='us-west-2')
         s3_resource = ses.resource('s3')
 
-        obj = s3_resource.Bucket(self.bucket)
+        obj = s3_resource.Bucket("test-aws-boto")
         print(ses)
 
-        with self.output().open('w') as output_file:
-        	file =  pq.read_table('On_Time_Reporting.parquet')
+        #with self.output().open('w') as output_file:
+        	#file =  pq.read_table('On_Time_Reporting.parquet')
             #output_file.write("raw,luigi,s3")
+
+        with self.output().open('w') as output_file:
+            output_file.write("On_Time_Reporting,luigi,s3")
 
 
     def output(self):
-        output_path = "s3://{}/raw.csv".\
-        format(self.bucket)#,
-        #self.root_path,
-        #self.task_name,
-        #self.year,
-        #str(self.month))
+        output_path = "s3://test-aws-boto/On_Time_Reporting.parquet"
 
-        #return luigi.local_target.LocalTarget('/home/silil/Documents/itam/metodos_gran_escala/data-product-architecture/luigi/test.csv')
         return luigi.contrib.s3.S3Target(path=output_path)
