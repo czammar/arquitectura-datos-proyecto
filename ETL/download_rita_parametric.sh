c2e9e3c99847bebbe59993d46237b48a6523cace
Cesar Zamora Martinez <cesar_czm@ciencias.unam.mx>
2020-03-04 16:19:04 -0600

    Script para descargar la base RITA completa, hasta 12/2019

---
+++ ETL/download_rita_parametric.sh
@@ -0,0 +1,15 @@
+# Script para descargar la base completa de RITA hasta
+# 12/2019 (ultima fecha de publicacion conocida)
+
+
+BASE_URL="https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_"
+
+function download_data() {
+    for url in "${BASE_URL}"{1987..2019}"_"{1..12}".zip"
+    do
+        echo ${url}
+        wget ${url}
+    done
+}
+
+download_data
