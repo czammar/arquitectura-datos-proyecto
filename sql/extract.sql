df375baf2a7f3858b2fb78c4b439c58d8b06dbb7
lgarayva <lgarayva@gmail.com>
2020-03-19 19:50:44 -0600

    separe los archivos

---
+++ sql/extract.sql
@@ -0,0 +1,13 @@
+###EXTRACT
+
+CREATE TABLE extract(
+  fecha DATE,
+  parametros CHAR(30),
+  usuario CHAR(20),
+  ip_ec2 CHAR(20),
+  num_renglones CHAR(10),
+  nombre_archivo CHAR(20),
+  ruta_s3 CHAR(30),
+  variable_datos CHAR(30)
+);
+
---
+++ sql/load.sql
@@ -0,0 +1,14 @@
+###LOAD
+
+
+CREAT TABLE load(
+  fecha DATE,
+  usuario CHAR(20),
+  ip_ec2 CHAR(20),
+  where_exec CHAR(20),
+  exec CHAR(20),
+  nums_obs_up CHAR(10),
+  status CHAR(10)
+)
+;
+        
