afd095ea8625669070ba13169a74fb412bff2767
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-03-19 18:30:25 -0600

    nueva version del orquestador (parametrico + carpetas)

--- ETL/new_orquestador.py
+++ ETL/new_orquestador.py
@@ -3,7 +3,7 @@
 # existe un bug con bot3 y luigi para pasar las credenciales
 # necesitas enviar el parámetro AWS_PROFILE e indicar el profile
 # con el que quieres que se corra
-# PYTHONPATH='.' AWS_PROFILE=dpa luigi --module orquestador downloadDataS3 --local-scheduler ...
+# PYTHONPATH='.' AWS_PROFILE=dpa_Danahi_c luigi --module new_orquestador downloadDataS3 --local-scheduler --year 1988 --month 11
 import luigi
 import luigi.contrib.s3
 import boto3
@@ -28,8 +28,8 @@ class downloadDataS3(luigi.Task):
     BASE_URL="https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_"
 
     #Definimos el mes y el anio base
-    YEAR=1988
-    MONTH=10
+    year = luigi.Parameter()
+    month = luigi.Parameter()
 
     def run(self):
         # Autenticación en S3
@@ -40,16 +40,18 @@ class downloadDataS3(luigi.Task):
         print(ses)
 
         #Formamos url_act que es el url actualizado
-        url_act = self.BASE_URL+str(self.YEAR)+"_"+str(self.MONTH)+".zip"
-        
+        url_act = self.BASE_URL+str(self.year)+"_"+str(self.month)+".zip"
+
         #Leemos los datos de la API, relativos al archivo en formato zip del periodo en cuestion
         r=requests.get(url_act)
+        data=r.content # resultado de la peticion a la API de Rita, en binario
 
         # Escribimos el archivo al bucket
-        # Nota: r.content es el resultado de la peticion a la API de Rita, en binario
-        obj.put_object(Key=str(self.YEAR)+"_"+str(self.MONTH)+".zip",Body=r.content)
+        output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".zip"
+        obj.put_object(Key=output_path,Body=r.content)
 
     def output(self):
         # Ruta en donde se guarda el archivo solicitado
-        output_path = "s3://test-aws-boto/" + str(self.YEAR)+"_"+str(self.MONTH)+".zip"
+        output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".zip"
+
         return luigi.contrib.s3.S3Target(path=output_path)
