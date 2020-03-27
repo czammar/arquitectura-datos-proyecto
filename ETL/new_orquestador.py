3695705e1479df1fc47baee9567f5661adc988f6
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-03-26 19:25:51 -0600

    se agrega informacion para obtener usuario de linux

--- ETL/new_orquestador.py
+++ ETL/new_orquestador.py
@@ -7,9 +7,18 @@
 import luigi
 import luigi.contrib.s3
 import boto3
+import getpass # Usada para obtener el usuario
 
 #importamos las librerias
 import requests
+from io import BytesIO
+from zipfile import ZipFile
+
+
+usuario = getpass.getuser() # Obtenemos el usuario
+#print(usuario)
+
+
 
 def create_bucket():
     ses = boto3.session.Session(profile_name='dpa_Danahi_c', region_name='us-west-2')
@@ -53,5 +62,45 @@ class downloadDataS3(luigi.Task):
     def output(self):
         # Ruta en donde se guarda el archivo solicitado
         output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".zip"
+
+        return luigi.contrib.s3.S3Target(path=output_path)
+
+
+class WritingCSVtoS3(luigi.Task):
+
+    #Parametros
+    task_name = "load"
+    #Definimos el mes y el anio base
+    year = luigi.Parameter()
+    month = luigi.Parameter()
+
+    #def requires(self):
+        #return downloadDataS3()
+
+    def run(self):
+
+        zip_key = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".zip"
+
+        ses = boto3.session.Session(profile_name='dpa_Danahi_c', region_name='us-west-2')
+        s3_resource = ses.resource('s3')
+
+        bucket_name = "test-aws-boto"
+        obj = s3_resource.Bucket("test-aws-boto")
+        print(ses)
+
+        # hola
+        #s3_resource = boto3.resource('s3')
+        zip_obj = s3_resource.Object(bucket_name=bucket_name, key=zip_key)
+        buffer = BytesIO(zip_obj.get()["Body"].read())
+
+        z = ZipFile(buffer)
+
+        zip_name="On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_"+str(self.year)+"_"+str(self.month)+".csv"
+
+        s3_resource.meta.client.upload_fileobj(z.open(zip_name), Bucket=bucket_name, Key=f'{zip_name}')
+
+
+    def output(self):
+        output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".csv"
 
         return luigi.contrib.s3.S3Target(path=output_path)
