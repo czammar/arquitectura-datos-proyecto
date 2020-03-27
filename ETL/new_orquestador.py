3e5326cb59e07889ea04e31e275fcc016a924ebf
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-03-26 20:56:24 -0600

    incluimos clase de metadatos raw

--- ETL/new_orquestador.py
+++ ETL/new_orquestador.py
@@ -16,9 +16,9 @@ from zipfile import ZipFile
 
 
 usuario = getpass.getuser() # Obtenemos el usuario
+
+
 #print(usuario)
-
-
 
 def create_bucket():
     ses = boto3.session.Session(profile_name='dpa_Danahi_c', region_name='us-west-2')
@@ -30,7 +30,33 @@ def create_bucket():
                 CreateBucketConfiguration={'LocationConstraint': 'us-west-2'},
                 ACL='private')
 
-#create_bucket()
+#create_bucket():
+
+
+
+# Preparamamos una clase para reunir los metadatos de la etapa Raw
+
+
+class Linaje_raw():
+    def __init__(self, fecha=0, nombre_task=0, parametros=0, usuario=0, ip_ec2=0, tamano_zip=0, nombre_archivo=0, ruta_s3=0):
+        self.fecha = fecha # time stamp
+        self.nombre_task = nombre_task
+        self.parametros = parametros # ver si se obtiene como lista
+        self.usuario = usuario # Usuario de la maquina de GNU/Linux que corre la instancia
+        self.ip_ec2 = ip_ec2
+        self.tamano_zip = tamano_zip
+        self.nombre_archivo = nombre_archivo
+        self.ruta_s3= ruta_s3
+
+    def to_upsert(self):
+        print (self.fecha, self.nombre_task, self.parametros, self.usuario, self.ip_ec2, self.tamano_zip, self.nombre_archivo, self.ruta_s3)
+
+# Inicializamos
+MiLinaje = Linaje_raw()
+
+#MiLinaje.to_upsert()
+
+
 
 class downloadDataS3(luigi.Task):
     #Definimos los URL base para poder actualizarlo automaticamente despues
@@ -39,6 +65,11 @@ class downloadDataS3(luigi.Task):
     #Definimos el mes y el anio base
     year = luigi.Parameter()
     month = luigi.Parameter()
+
+    # Intentamos recoger parametros
+    MiLinaje.fecha = "Ya tengo hambre :("#luigi.Parameter().parse(year)# :D :D :D
+    MiLinaje.usuario = getpass.getuser()
+    MiLinaje.to_upsert()
 
     def run(self):
         # Autenticación en S3
@@ -89,7 +120,7 @@ class WritingCSVtoS3(luigi.Task):
         print(ses)
 
         # hola
-        #s3_resource = boto3.resource('s3')
+        s3_resource = boto3.resource('s3')
         zip_obj = s3_resource.Object(bucket_name=bucket_name, key=zip_key)
         buffer = BytesIO(zip_obj.get()["Body"].read())
 
@@ -101,6 +132,6 @@ class WritingCSVtoS3(luigi.Task):
 
 
     def output(self):
-        output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".csv"
+        output_path = "s3://test-aws-boto/YEAR="+str(self.year)+"/MONTH="+str(self.month)+"/"+str(self.year)+"_"+str(self.month)+".zip"
 
         return luigi.contrib.s3.S3Target(path=output_path)
