e590132e74b9adc2504d19da1ed67155c85f9f14
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-03-27 22:26:25 -0600

    intento fallido de conectar con psycopg2

--- ETL/create_rds.py
+++ ETL/create_rds.py
@@ -1,9 +1,8 @@
 #CLASE MASTER DE AWS
 
-import boto3
 from botocore.config import Config
 import boto3
-import botocore
+#import botocore
 import sys
 import random
 import time
@@ -41,7 +40,8 @@ def create_db(nuevo_id):
              "DBInstanceClass":'db.t2.micro',
              "Engine":'postgres',
              "AllocatedStorage":5,
-             "Port":5432
+             "Port":5432#,
+             #"DBName":'metadatos'
         }
         rds_client.create_db_instance(**db_vars)
     except Exception as error:
@@ -57,9 +57,41 @@ def delete_db(id_borrar):
         print (error)
 
 # MAIN
-#describe_db()
+
 #create_db('metadatos')
+#describe_db()
 #delete_db('metadatos')
-describe_db()
+#delete_db('postgres')
+#describe_db()
 #create_db()
 #describe_db()
+
+print("*************  Informacion relevante  *************")
+dbs = rds_client.describe_db_instances()
+host=dbs['DBInstances'][0].get('Endpoint').get('Address')
+port=dbs['DBInstances'][0].get('Endpoint').get('Port')
+
+# Obtiene Endpoint de la base de datos
+#print (dbs['DBInstances'][0].get('Endpoint').get('Address'))
+# Obtiene Port de la base de datos
+#print (dbs['DBInstances'][0].get('Endpoint').get('Port'))
+
+import psycopg2
+
+
+connection = psycopg2.connect(user="dpa", # Usuario RDS
+                              password="dpa01_largo", # password de usuario de RDS
+                              host=host,#"127.0.0.1", # cambiar por el endpoint adecuado
+                              port="5432", # cambiar por el puerto
+                              database="postgres") # Nombre de la base de datos
+
+
+cursor = connection.cursor()
+postgreSQL_select_Query = "CREATE TABLE extract( fecha DATE, parametros CHAR(30), usuario CHAR(20),\
+  ip_ec2 CHAR(20), num_renglones CHAR(10), nombre_archivo CHAR(20),\
+  ruta_s3 CHAR(30), variable_datos CHAR(30) )"
+
+cursor.execute(postgreSQL_select_Query)
+cursor.close()
+connection.close()
+print("PostgreSQL connection is closed")
