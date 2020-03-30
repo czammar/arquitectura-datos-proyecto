bb3d87e76991c53963d247cec8ffc41202c27dc9
Cesar Zamora Martinez <44296157+czammar@users.noreply.github.com>
2020-03-30 17:33:06 -0600

    Agregamos sudo al file

--- UTILS/rita.sh
+++ UTILS/rita.sh
@@ -12,11 +12,11 @@
 # Despues de correrlo, en la terminal correr
 # source ~/.bashrc
 
-apt update
+sudo apt update
 #sudo apt update
 # Instalamos librerias necesarias para pyenv y otras utilidades
 
-apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
  libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev\
   xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 #sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
@@ -24,10 +24,10 @@ apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
   #xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 cd ~
 
-apt-get install -y  libpq-dev
+sudo apt-get install -y  libpq-dev
 
 
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+sudo curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 echo '********* Iniciamos modificacion del bashrc ************'
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
