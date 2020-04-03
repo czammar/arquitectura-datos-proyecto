96018397c79847d5ddc37ae0443c7d5f6af8b2df
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-03 11:45:31 -0600

    eliminamos comentarios que ya no se usan

---
+++ UTILS/new_rita.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# Script para descargar pyenv y virtualenv, para crear ambiente donde se corre luigi
+sudo apt update
+
+# Instalamos librerias necesarias para pyenv y otras utilidades
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev\
+ libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev\
+  xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+cd ~
+
+<<<<<<< HEAD
+sudo apt-get install -y libpq-dev postgresql postgresql-contrib # instala postgresql y psql
+=======
+sudo apt-get install -y libpq-dev postgresql postgresql-contrib # Librerias para postgreSQL y psql
+
+>>>>>>> d6eaf67bd40c256603a395cc5bad0e56d39310eb
+
+# Pyenv y virtualenv
+sudo curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+echo '********* Iniciamos modificacion del bashrc ************'
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+echo '********* Final de modificacion del bashrc +************'
