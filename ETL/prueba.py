3cb42b33a170916718f7cb6bf1fdb8c109f02acc
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-09 18:31:57 -0500

    borrando contado y prueba para dpa

--- ETL/prueba.py
+++ ETL/prueba.py
@@ -1,6 +1,6 @@
 import boto3
 
-ses = boto3.session.Session(profile_name="dpa_Danahi_c", region_name='us-west-2')
+ses = boto3.session.Session(profile_name="dpa", region_name='us-west-2')
 s3 = ses.resource('s3')
 
 # listar los buckets que están en este perfil y región.
@@ -12,5 +12,5 @@ my_bucket = s3.Bucket(bucket_name )
 for my_bucket_object in my_bucket.objects.all():
     print(my_bucket_object, my_bucket_object.size)
 
-tam = my_bucket.Object(key='1988_10.zip').content_length
-print(tam)
+#tam = my_bucket.Object(key='1988_10.zip').content_length
+#print(tam)
