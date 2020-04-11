34e4fbcfeabb111fbd8ef25cb47fc934c6bb2075
cesar zamora martinez <cesar_czm@ciencias.unam.mx>
2020-04-10 19:48:29 -0500

    version alterna de orquestador y modulo de utlidades para metadatos EL

--- ETL/task_insert_extract_metadata_luigi_postgres.py
+++ ETL/task_insert_extract_metadata_luigi_postgres.py
@@ -44,3 +44,58 @@ class InsertExtractMetada(CopyToTable):
         # Insertamos renglones en tabla
         for element in r:
             yield element
+
+def EL_verif_query():
+    # Conexion a BD y query para verificacion
+    # Lectura de archivo de credenciales en directorio (no subirlo a git)
+    credentials = pd.read_csv("postgres_credentials.csv")
+    user = credentials.user[0]
+    password = credentials.password[0]
+    database = credentials.database[0]
+    host = credentials.host[0]
+
+    # Conexion y cursor para query
+    connection = psycopg2.connect(user=user, # Usuario RDS
+                                 password=password, # password de usuario de RDS
+                                 host=host,# endpoint
+                                 port="5432", # cambiar por el puerto
+                                 database=database) # Nombre de la base de datos
+    cursor = connection.cursor()
+
+    # Query para verificacion a la base de datos
+    postgreSQL_select_Query = "SELECT * from metadatos.extract WHERE task_status = '" + str(url_act) + "';"
+    cursor.execute(postgreSQL_select_Query)
+    print("Query de verificacion")
+    select_Query = cursor.fetchall()
+    tam = len(select_Query)
+    cursor.close()
+    connection.close()
+    print("PostgreSQL connection is closed")
+
+    return tam
+
+def EL_metadata():
+
+    # Lectura de archivo de credenciales en directorio (no subirlo a git)
+    credentials = pd.read_csv("postgres_credentials.csv")
+    user = credentials.user[0]
+    password = credentials.password[0]
+    database = credentials.database[0]
+    host = credentials.host[0]
+
+    connection = psycopg2.connect(user=user, # Usuario RDS
+                                 password=password, # password de usuario de RDS
+                                 host=host,# endpoint
+                                 port="5432", # cambiar por el puerto
+                                 database=database) # Nombre de la base de datos
+    cursor = connection.cursor()
+
+    # Query para insertar metadatos
+    postgres_insert_query = """ INSERT INTO metadatos.extract (fecha, nombre_task, year, month, usuario, ip_ec2, tamano_zip, nombre_archivo, ruta_s3, task_status) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s ) """
+    record_to_insert = MiLinaje.to_upsert()
+    cursor.execute(postgres_insert_query, record_to_insert)
+    connection .commit()
+    cursor.close()
+    connection.close()
+
+    return print("Metadadata Insertion Done - PostgreSQL connection is closed")
